402ed44de485b550a597c3a85fcc6320f258966f
rhyttr <rhyttr@gmail.com>
2016-05-12 17:50:04 +0800

    setction 7

---
+++ client/main.js
@@ -0,0 +1,5 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+import { Session } from 'meteor/session';
+
+Session.set('pageTitle', 'VXDIAG Microscope');
