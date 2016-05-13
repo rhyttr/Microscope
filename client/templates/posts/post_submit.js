c9ef5f1cd29eadf70291102c60bf89d29da4472e
rhyttr <rhyttr@gmail.com>
2016-05-13 18:09:17 +0800

    Section 9.5

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -1,3 +1,15 @@
+Template.postSubmit.onCreated(function() {
+  Session.set('postSubmitErrors', {});
+});
+Template.postSubmit.helpers({
+  errorMessage: function(field) {
+    return Session.get('postSubmitErrors')[field];
+  },
+  errorClass: function (field) {
+    return !!Session.get('postSubmitErrors')[field] ? 'has-error' : '';
+  }
+});
+
 Template.postSubmit.events({
   'submit form': function(e) {
     e.preventDefault();
@@ -7,14 +19,18 @@ Template.postSubmit.events({
       title: $(e.target).find('[name=title]').val()
     };
 
+    var errors = validatePost(post);
+    if (errors.title || errors.url)
+      return Session.set('postSubmitErrors', errors);
+
     Meteor.call('postInsert', post, function(error, result) {
       // error msg and return
       if (error)
-        return alert(error.reason);
+        return throwError(error.reason);
 
       // 显示结果，跳转页面
       if (result.postExists)
-        alert('This link has already been posted（该链接已经存在）');
+        throwError('This link has already been posted（该链接已经存在）');
 
       //Router.go('postPage', {_id: result._id});
       Router.go('postsList');
