79b6af9bcab31034d0f94ebedb6a3123f73aaeb0
rhyttr <rhyttr@gmail.com>
2016-05-21 16:17:30 +0800

    Section 10

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,21 +1,67 @@
-if (Posts.find().count() === 0) {
-  Posts.insert({
-    title: 'Introducing Telescope',
-    url: 'http://sachagreif.com/introducing-telescope/'
+if (Posts.find().count() === 1) {
+
+  var now = new Date().getTime();
+
+  // create two users
+  var tomId = Meteor.users.insert({
+    profile: { name: 'Tom Coleman' }
   });
+  var tom = Meteor.users.findOne(tomId);
+  var sachaId = Meteor.users.insert({
+    profile: { name: 'Sacha Greif' }
+  });
+  var sacha = Meteor.users.findOne(sachaId);
+
+  var telescopeId = Posts.insert({
+    title: 'Introducing VXDIAG.COM',
+    userId: sacha._id,
+    author: sacha.profile.name,
+    url: 'http://www.vxdiag.com/',
+    submitted: new Date(now - 7 * 3600 * 1000),
+    commentsCount: 2
+  });
+
+  Comments.insert({
+    postId: telescopeId,
+    userId: tom._id,
+    author: tom.profile.name,
+    submitted: new Date(now - 5 * 3600 * 1000),
+    body: 'Interesting project Sacha, can I get involved?'
+  });
+
+  Comments.insert({
+    postId: telescopeId,
+    userId: sacha._id,
+    author: sacha.profile.name,
+    submitted: new Date(now - 3 * 3600 * 1000),
+    body: 'You sure can Tom!'
+  });
+
 
   Posts.insert({
     title: 'Meteor',
-    url: 'http://meteor.com'
+    userId: tom._id,
+    author: tom.profile.name,
+    url: 'http://meteor.com',
+    submitted: new Date(now - 10 * 3600 * 1000),
+    commentsCount: 0
   });
 
   Posts.insert({
     title: 'The Meteor Book',
-    url: 'http://themeteorbook.com'
+    userId: tom._id,
+    author: tom.profile.name,
+    url: 'http://themeteorbook.com',
+    submitted: new Date(now - 12 * 3600 * 1000),
+    commentsCount: 0
   });
-  
+
   Posts.insert({
-    title: 'VXDIAG',
-    url: 'http://www.vxdiag.net'
+    title: 'VXDIAG.net',
+    userId: tom._id,
+    author: tom.profile.name,
+    url: 'http://www.vxdiag.net',
+    submitted: new Date(now - 12 * 3600 * 1000),
+    commentsCount: 0
   });
 }
